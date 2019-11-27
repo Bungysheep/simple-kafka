407d95fff48567cc723786bbba5fa0a8d50eac68
James Embongbulan <james.embongbulan@gmail.com>
2019-11-27 22:52:58 +0800

    Consumer thread

--- src/main/java/com/github/bungyesheep/consumer/ConsumerDemo.java
+++ src/main/java/com/github/bungyesheep/consumer/ConsumerDemo.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,33 +12,96 @@ import org.slf4j.LoggerFactory;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
 
 public class ConsumerDemo {
-    public static void main(String[] args) {
+    public void Run() {
         Logger logger = LoggerFactory.getLogger(ConsumerDemo.class.getName());
 
-        // Create consumer configs
-        Properties properties = new Properties();
-        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
-        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group-1");
-        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        // Latch to dial with multi thread
+        CountDownLatch latch = new CountDownLatch(1);
 
-        // Create consumer
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+        // Create the consumer runnable
+        logger.info("Creating the consumer thread");
+        Runnable myConsumerRunnable = new ConsumerThread(latch);
 
-        // Subscribe consumer to topic
-        consumer.subscribe(Collections.singleton("topic-1"));
+        // Start the thread
+        Thread myThread = new Thread(myConsumerRunnable);
+        myThread.start();
 
-        // Poll for new data
-        while(true) {
-            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+        // Add a shutdown hock
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            logger.info("Caught shutdown hock");
+            ((ConsumerThread)myConsumerRunnable).shutdown();
 
-            for (ConsumerRecord record: records) {
-                logger.info("Key: "+record.key() +", Value: "+record.value());
-                logger.info("Partition: "+record.partition() +", Offset: "+record.offset());
+            try {
+                latch.await();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
+
+            logger.info("Application has exited");
+        }));
+
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            logger.error("Application got interrupted", e);
+        } finally {
+            logger.info("Application is closing");
+        }
+    }
+
+    public static void main(String[] args) {
+        new ConsumerDemo().Run();
+    }
+
+    public class ConsumerThread implements Runnable {
+
+        private  CountDownLatch latch;
+        private KafkaConsumer<String, String> consumer;
+        private Logger logger = LoggerFactory.getLogger(ConsumerThread.class.getName());
+
+        public ConsumerThread(CountDownLatch latch) {
+            // Create consumer configs
+            Properties properties = new Properties();
+            properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+            properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+            properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+            properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "group-2");
+            properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+            // Create consumer
+            this.consumer = new KafkaConsumer<String, String>(properties);
+
+            // Subscribe consumer to topic
+            consumer.subscribe(Collections.singleton("topic-1"));
+
+            this.latch = latch;
+        }
+
+        @Override
+        public void run() {
+            try {
+                // Poll for new data
+                while(true) {
+                    ConsumerRecords<String, String> records = this.consumer.poll(Duration.ofMillis(100));
+
+                    for (ConsumerRecord record: records) {
+                        this.logger.info("Key: "+record.key() +", Value: "+record.value());
+                        this.logger.info("Partition: "+record.partition() +", Offset: "+record.offset());
+                    }
+                }
+            } catch (WakeupException e) {
+                this.logger.info("Received shutdown signal!");
+            } finally {
+                this.consumer.close();
+                this.latch.countDown();
+            }
+        }
+
+        public void shutdown() {
+            this.consumer.wakeup();
         }
     }
 }
