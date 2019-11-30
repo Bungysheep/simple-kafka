3db357a5510ab8d29e14111de0ab386e28c4dbb4
James Embongbulan <james.embongbulan@gmail.com>
2019-11-30 12:56:02 +0800

    Elasticsearch consumer

--- src/main/java/com/github/bungysheep/consumer/ConsumerDemo.java
+++ src/main/java/com/github/bungysheep/consumer/ConsumerDemo.java
@@ -1,4 +1,4 @@
-package com.github.bungyesheep.consumer;
+package com.github.bungysheep.consumer;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
